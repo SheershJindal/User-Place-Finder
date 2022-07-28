1ca68bdd9514593e5ab364beb85694bf04f56d49
Sheersh Jindal <sheershjindal@gmail.com>
2022-07-28 22:46:07 +0530

    Added New place page, places for specific user, Improved mobile side drawer

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import PlaceList from '../components/PlaceList'
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Taj Mahal',
+        description: 'One of 7 wonders of world',
+        imageUrl: 'https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-750-20151104113424.jpg',
+        address: 'Agra',
+        location: {
+            lat: 213.45543,
+            lang: 134.21345
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Taj Mahal',
+        description: 'One of 7 wonders of world',
+        imageUrl: 'https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-750-20151104113424.jpg',
+        address: 'Agra',
+        location: {
+            lat: 213.45543,
+            lang: 134.21345
+        },
+        creator: 'u2'
+    }
+]
+
+const UserPlaces = props => {
+
+    const userId = useParams().userId
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+    return (
+        <PlaceList items={loadedPlaces} />
+    )
+}
+
+export default UserPlaces
